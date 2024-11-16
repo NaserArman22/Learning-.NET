5b4971cf90b4d4e4825a6ee6b42adfd3cdc021b4
NaserArman22 <mdnaserarman57@gmail.com>
2024-11-17 00:14:41 +0600

    without validation

---
+++ form/form/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace form.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+       //HttpRequestBase
+        public ActionResult getForm()
+        {
+            Student s = new Student();
+            s.Name = Request["Name"];
+            s.Id = Request["Id"];
+            s.CGPA = Request["CGPA"];
+            return View(s);
+        }
+        //variable name mapping\
+        //[HttpPost]
+        /*public ActionResult getForm(string Name, string Id, string CGPA)
+        {
+            Student s=new Student();
+            s.Name = Name;
+            s.Id = Id;
+            s.CGPA = CGPA;
+            return View(s);
+        }*/
+        /*FormCollection
+        [HttpPost]
+        public ActionResult getForm(FormCollection form)
+        {
+            Student s= new Student();
+            s.Name = form["Name"];
+            s.Id=form["Id"];
+            s.CGPA = form["CGPA"];
+            return View();
+        }*/
+    }
+    
+}
---
+++ form/form/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace form.Models
+{
+    public class Student
+    {
+        public string Name {  get; set; }
+        public string Id { get; set; }
+        public string CGPA { get; set; }
+    }
+}
